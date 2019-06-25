7e1bd1795bcd59f81285678455a8d5d9e598e593
falakh <zulfikarfahmi38@gmail.com>
2019-06-25 09:49:06 +0700

    fix db

--- server/database.js
+++ server/database.js
@@ -1,34 +1,53 @@
 /* eslint-disable no-console */
 const Connection = require('tedious').Connection
-    // eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
 const Request = require('tedious').Request
 
 // Create connection to database
 const config = {
-    authentication: {
-        options: {
-            userName: 'dicoding', // update me
-            password: 'Fahmi1234567890', // update me
-            connectTimeout: 0
-        },
-        type: 'default'
-    },
-    server: 'dicodingbayar.database.windows.net', // update me
+  authentication: {
     options: {
-        database: 'sportest', // update me
-        encrypt: false
-    }
+      userName: 'dicoding', // update me
+      password: 'Fahmi1234567890' // update me
+    },
+    type: 'default'
+  },
+  server: 'dicodingbayar.database.windows.net', // update me
+  options: {
+    database: 'sportest', // update me
+    encrypt: true
+  }
 }
 const connection = new Connection(config)
 
 // Attempt to connect and execute queries if connection goes through
 connection.on('connect', function(err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('sukses')
-            // queryDatabase();
-    }
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('sukses')
+    // queryDatabase();
+  }
 })
 
+function queryDatabase(onClear) {
+  console.log('Reading rows from the Table...')
+
+  // Read all rows from table
+  // eslint-disable-next-line handle-callback-err
+  const request = new Request('SELECT * FROM USERS', function(
+    err,
+    rowCount,
+    rows
+  ) {
+    console.log(rowCount + ' row(s) returned')
+  })
+
+  request.on('row', function(columns) {
+    onClear(columns)
+  })
+  connection.execSql(request)
+}
+
 module.exports = connection
+module.exports.runSelect = queryDatabase
